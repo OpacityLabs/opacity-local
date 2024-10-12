76dc99b641a145b203b40b840573c1686831e354
Ron Turetzky <turetzkyron@gmail.com>
2024-10-12 18:09:01 +0300

    chore: amending script to insert the operators address and ip

--- docker/node/start.sh
+++ docker/node/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# address=0x$(cat /opacity-avs-node//opacity.bls.key.local.json | jq -r '.address')
-# yq -i ".operator_address = \"$address\"" opacity.config.yaml 
+address=0x$(cat /opacity-avs-node/config/opacity.ecdsa.key.json | jq -r '.address')
+sed -i "s/^operator_address:.*/operator_address: \"$address\"/" /opacity-avs-node/config/opacity.config.yaml
+sed -i "s/^node_public_ip:.*/node_public_ip: \$NODE_IP\"/" /opacity-avs-node/config/opacity.config.yaml
 cargo run --bin register /opacity-avs-node/config/opacity.config.yaml 
 cargo run --bin opacity-avs-node --release -- --config-file /opacity-avs-node/config/config.yaml
