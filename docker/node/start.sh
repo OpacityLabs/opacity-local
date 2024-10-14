7b24234835d6e3f375f71fa9a0828d0cf854fd53
Ron Turetzky <turetzkyron@gmail.com>
2024-10-14 14:12:17 +0300

    chore: opt node start scritp and amending config to avoid yq corruption

--- docker/node/start.sh
+++ docker/node/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 address=0x$(cat /opacity-avs-node/config/opacity.ecdsa.key.json | jq -r '.address')
-sed -i "s/^operator_address:.*/operator_address: \"$address\"/" /opacity-avs-node/config/opacity.config.yaml
-sed -i "s/^node_public_ip:.*/node_public_ip: \$NODE_IP\"/" /opacity-avs-node/config/opacity.config.yaml
-cargo run --bin register /opacity-avs-node/config/opacity.config.yaml 
-cargo run --bin opacity-avs-node --release -- --config-file /opacity-avs-node/config/config.yaml
+yq -i -Y ".operator_address = \"$address\"" /opacity-avs-node/config/opacity.config.yaml
+yq -i -Y ".node_public_ip = \"$NODE_IP\"" /opacity-avs-node/config/opacity.config.yaml
+/opacity-avs-node/target/release/register /opacity-avs-node/config/opacity.config.yaml 
+/opacity-avs-node/target/release/opacity-avs-node --config-file /opacity-avs-node/config/config.yaml
